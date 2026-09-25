79428b2d0269b370c867831711ad86dca366f489
Jakhongir <jumaevjakhongir6801@gmail.com>
2026-09-25 19:24:18 +0900

    feat: develop member schema model

---
+++ src/schema/Member.module.ts
@@ -0,0 +1,28 @@
+import mongoose, { Schema } from 'mongoose';
+import {MemberStatus, MemberType} from "../libs/enums/member.enum"
+
+const memberSchema = new Schema ({
+    memberType: {
+        type: String,
+        enum: MemberType,
+        default: MemberType.USER,
+    },
+
+    memberStatus:{
+        type: String,
+        enum: MemberStatus,
+        default: MemberStatus.ACTIVE
+    },
+
+    memberNick:{
+        type: String,
+        index: {unique:true, sparse:true},
+        required: true,
+    },
+
+    memberPhone:{
+        type:String,
+        index:{unique:true, sparse:true},
+        required: true;
+    }
+});
